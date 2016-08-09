9e28e7bc3ae3a354d4ce67a1b359337ed8e4d950
James Strachan <james.strachan@gmail.com>
2016-08-09 14:34:26 +0100

    fixes https://github.com/fabric8io/ipaas-quickstarts/issues/1363

--- src/main/java/io/fabric8/quickstarts/hystrix/Application.java
+++ src/main/java/io/fabric8/quickstarts/hystrix/Application.java
@@ -19,6 +19,9 @@ package io.fabric8.quickstarts.hystrix;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 @EnableCircuitBreaker
@@ -26,5 +29,16 @@ public class Application {
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
+    }
+
+
+    @Configuration
+    public static class SpringConfig {
+        // This is often created for you for some spring cloud starters
+
+        @Bean
+        public RestTemplate restTemplate() {
+            return new RestTemplate();
+        }
     }
 }
